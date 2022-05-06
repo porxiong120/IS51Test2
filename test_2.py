3a801eb767b9a925bdb22aad224688e79fda8473
Por Xiong <porxiong2020@outlook.com>
2022-05-06 12:37:44 -0700

    adding structured-english

---
+++ test_2.py
@@ -0,0 +1,12 @@
+"""
+The program would like to display the number of grades, the average 
+grade, and the percentage of grades that are above the average grade
+by accessing a file named Final.txt, which contains student grades
+on the final exam. The program will be using two function. 
+
+The file will be open to get the information and closed after the 
+information have been extracted.
+The first function named main will start the application. 
+The second function named calculate percent above average will 
+calculate the percentage of grades that are above the average grade. 
+"""
